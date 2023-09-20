14aae4aacb2ce3489eab67ab8d94742ed43c7da6
Biftu S <biftushibbire@gmail.com>
2023-09-20 18:41:29 +0300

    Initial commit: Initial files added

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./src/**/*.js', './src/**/*.jsx', './src/**/*.html'],
+  theme: {
+    fontFamily: {
+      cursive: ['CursiveFontName', 'cursive', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
