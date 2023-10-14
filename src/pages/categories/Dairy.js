8935bcf424482ad3a2a89f8bb2e14e7423cdcede
Biftu S <biftushibbire@gmail.com>
2023-10-14 03:00:16 +0300

    modified

--- src/pages/categories/Dairy.js
+++ src/pages/categories/Dairy.js
@@ -44,7 +44,7 @@ const Dairy = ({ cartItems, setCartItems }) => {
           </div>
           <div className=" p-4">
             <h2 className="text-2xl font-bold mb-4">Dairy Products</h2>
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-4 ">
+            <div className="grid grid-cols-1  sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-4 ">
               {filteredProducts.map((product, index) => (
                 <div
                   key={index}
--- src/pages/categories/Meat and Seafoods.js
+++ src/pages/categories/Meat and Seafoods.js
@@ -4,15 +4,14 @@ import { categories } from './Category';
 import { Link } from 'react-router-dom';
 
 import Search from '../Search';
-const Meat  = ({ cartItems, setCartItems }) => {
+
+const Meat = ({ cartItems, setCartItems }) => {
   const handleAddToCart = (product) => {
     setCartItems([...cartItems, product]);
     console.log(`Added ${product.name} to cart`);
   };
 
   const [searchQuery, setSearchQuery] = useState('');
-
- 
 
   const handleSearch = (query) => {
     setSearchQuery(query);
@@ -21,15 +20,16 @@ const Meat  = ({ cartItems, setCartItems }) => {
   const filteredProducts = categories[4].products.filter((product) =>
     product.name.toLowerCase().includes(searchQuery.toLowerCase())
   );
+
   return (
     <>
       <Header />
-      <div className="fixed top-4 left-48 lg:w-1/3 md:w-1/4 sm:w-1/4 z-20 my-2">
+      <div className="fixed lg:top-4 md:top-6 sm:top-4 left-32 lg:w-1/3 md:w-1/4 sm:w-1/4 z-20 my-2">
         <Search onSearch={handleSearch} /> 
       </div>
-      <div className="bg-green-50 min-h-screen pt-6">
-        <div className="flex">
-          <div className="w-1/4 bg-green-100 p-4 m-8 rounded-md">
+      <div className="container mx-auto px-3">
+        <div className="flex flex-col md:flex-row">
+          <div className="md:w-1/4 bg-green-100 p-4 m-4 rounded-md">
             <h2 className="text-2xl font-bold mb-4">Categories</h2>
             <ul className="space-y-2">
               {categories.map((category, index) => (
@@ -42,13 +42,13 @@ const Meat  = ({ cartItems, setCartItems }) => {
               ))}
             </ul>
           </div>
-          <div className="w-3/4 p-4">
+          <div className=" p-4">
             <h2 className="text-2xl font-bold mb-4">Meat and Seafoods</h2>
-            <div className="grid grid-cols-3 gap-4 h-3/4">
+            <div className="grid grid-cols-1  sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-4 ">
               {filteredProducts.map((product, index) => (
                 <div
                   key={index}
-                  className="border p-4 rounded-lg hover:shadow-lg transition-shadow"
+                  className="border p-4 rounded-lg flex flex-col items-center justify-center hover:shadow-lg transition-shadow"
                 >
                   <img
                     src={product.image}
@@ -56,7 +56,7 @@ const Meat  = ({ cartItems, setCartItems }) => {
                     className="mb-2 h-56 rounded-lg"
                   />
                   <h3 className="text-lg font-bold">{product.name}</h3>
-                  <p className="text-gray-500">${product.price.toFixed(2)}</p>
+                  <p className="text-gray-500">{product.price.toFixed(2)} Birr</p>
                   <button
                     className="bg-green-500 text-white px-4 py-2 mt-2 rounded-lg hover:bg-green-600 transition-colors"
                     onClick={() => handleAddToCart(product)}
@@ -73,4 +73,4 @@ const Meat  = ({ cartItems, setCartItems }) => {
   );
 };
 
-export default Meat ;
+export default Meat;
